04fd420e2ee832ae48f171a872fa5f86fc6cfa10
Lenzh <lorenzo0109@hotmail.nl>
2018-12-20 01:04:40 +0100

    Create __resource.lua

---
+++ __resource.lua
@@ -0,0 +1,7 @@
+-----------------------------------------------------
+--- Advanced 911, Made by FAXES modified by Lenzh ---
+-----------------------------------------------------
+
+client_script "client.lua"
+
+server_script "server.lua"
